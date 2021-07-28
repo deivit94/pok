303ffd7096bdea1614ccfdb8840282808e8bc9a5
deivit94 <continodavidnicolas@gmail.com>
2021-07-27 21:20:51 -0300

    first commit

---
+++ docs/app.js
@@ -0,0 +1,57 @@
+async function searchPokemon() {
+    
+    try {
+        const inputValue = document.getElementById('input').value.toLowerCase(); 
+        const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
+        const json = await data.json();
+
+        const datacolor = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${inputValue}`);
+        const jsoncolor = await datacolor.json();
+        const pokecolor = jsoncolor.color.name;
+            
+        document.getElementById("bigBox").style.backgroundColor = pokecolor; 
+
+        document.getElementById('pokeImg').src = json.sprites.front_default;
+        document.getElementById('pokeImg').classList.add('img');
+        document.getElementById('pokeName').innerText = json.name;
+        document.getElementById('input').value = "";
+    } catch(err) {
+        document.getElementById('pokeImg').classList.add('img');
+        document.getElementById('pokeImg').src = "img/error.png";
+        document.getElementById('pokeName').innerText = 'error';
+    }
+}
+
+
+function enter(e) {
+    if (e.key == "Enter"){
+        searchPokemon();
+    }
+}
+
+
+
+
+
+
+
+
+
+// async function searchPokemon(e) {
+//     if (e.key == "Enter") {
+//     const inputValue = document.getElementById('input').value.toLowerCase(); 
+//     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
+//     const json = await data.json();
+    
+//     const datacolor = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${inputValue}`);
+//     const jsoncolor = await datacolor.json();
+//     const pokecolor = jsoncolor.color.name;
+//     document.getElementById("bigBox").style.backgroundColor = pokecolor; 
+
+//     document.getElementById('pokeImg').src = json.sprites.front_default;
+//     document.getElementById('pokeImg').classList.add('img');
+//     document.getElementById('pokeName').innerText = json.name;
+//     document.getElementById('input').value = "";
+// }
+// }
+
